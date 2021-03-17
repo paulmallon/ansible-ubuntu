e12dc2f355178c668d2821a1d76f8a633a3766c9
PM <paul.mallon@pricerunner.com>
2021-03-17 12:44:27 +0100

    Added some roles

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+###################
+# Install ansible #
+if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    echo "Adding Ansible PPA"
+    sudo apt-add-repository ppa:ansible/ansible -y
+fi
+
+if ! hash ansible >/dev/null 2>&1; then
+    echo "Installing Ansible..."
+    sudo apt-get update
+    sudo apt-get install software-properties-common ansible -y
+else
+    echo "Ansible already installed"
+fi
+
+
+echo "Run ansible with :"
+echo "  ansible-playbook ansible-desktop.yml --ask-become-pass"
+
+
+
